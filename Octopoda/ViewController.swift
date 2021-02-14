28c852c530d306311ad12aa4d3f07641d3aebea5
Mehmet Müjde <mehmet.muejde@me.com>
2021-02-14 02:41:34 +0100

    Initial Commit

---
+++ Octopoda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Octopoda
+//
+//  Created by Mehmet Müjde on 14.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
